9acdbafd1abf06a78a809d8011e285fd412f799d
Jan Świder <jasiek.swider@outlook.com>
2023-10-10 20:35:07 +0200

    Wersja  normal (3/3) ready

--- Labs/Lab1/script.js
+++ Labs/Lab1/script.js
@@ -1,23 +1,37 @@
-console.log("Hello");
+setEventListeners();
 
-const btnSubmit = document.querySelector("#BtnSubmit");
+const btnAddItem = document.querySelector("#Add");
+btnAddItem.addEventListener("click",function(){
+    const item = document.createElement("div");
+    const input = document.createElement("input");
+    const button = document.createElement("button");
+    
+    item.classList.add("item");
+    input.setAttribute("type","number");
+    button.innerText = "-";
 
-btnSubmit.addEventListener("click",solve);
+    item.appendChild(input);
+    item.appendChild(button);
 
-const inputs = document.querySelectorAll("input");
-inputs.forEach(element => {
-    element.addEventListener('change', solve);
+    btnAddItem.parentNode.insertBefore(item,btnAddItem);
+
+    setEventListeners();
 });
-
 
 function add(a, b) {
     return a + b;
   }
 
 function solve(){
-    const inputs = document.querySelectorAll("input");
-    const result = Array.from(inputs, ({ value }) => parseFloat(value));
-    
+    const inputs = Array.from(document.querySelectorAll("input"));
+    const validInputs = inputs.map(e => e.value).filter(e => e);
+    const result = Array.from(validInputs, (e) => parseFloat(e));
+    console.table(result);
+    if(result.length == 0){
+        clearOutput();
+        return;
+    }
+
     const min = Math.min.apply(null, result);// apply accepts an array and it applies the array as parameters to the actual function. So, Math.max.apply(Math, list); 
     const max = Math.max.apply(null, result);// can be understood as, Math.max("12", "23", "100", "34", "56", "9", "233");
     const sum = result.reduce(add,0); //reduce wywołuje funkcje na każdym elementem tablicy
@@ -47,4 +61,21 @@ function createOutput(min, max, sum, avg){
     resultDiv.appendChild(divAvg);
 }
 
+function clearOutput(){
+    const resultDiv = document.querySelector("#results");
+    resultDiv.innerHTML = "";
+}
 
+function setEventListeners(){
+    let items = document.querySelectorAll(".item");
+    items.forEach(item =>{
+        let deleteButton = item.querySelector("button")
+        deleteButton.addEventListener("click",function() {
+            item.remove();
+            solve();
+        });
+
+        let input = item.querySelector("input");
+        input.addEventListener('change', solve);
+    });
+}
